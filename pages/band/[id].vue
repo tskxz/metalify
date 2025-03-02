18665bb2b76baa257a7f14c87d92480c7cdc5161
Tanjil <tanjilkh@gmail.com>
2025-03-02 09:09:14 +0000

    feat: delete albums

--- pages/band/[id].vue
+++ pages/band/[id].vue
@@ -1,10 +1,13 @@
-<script setup>
-// TODO: Modal Apagar Albuns
+<script setup lang="ts">
+
 import { useRoute } from "vue-router";
 import { useAlbums } from "@/composables/useAlbums";
 import { useBand } from "@/composables/useBand";
 import { computed } from "vue";
 import {useRouter} from "vue-router";
+import {useRuntimeConfig} from "#app"
+
+const config = useRuntimeConfig()
 
 const route = useRoute();
 const bandId = Number(route.params.id);
@@ -21,6 +24,30 @@ const goBack = () => {
 const goToAddAlbum = () => {
   router.push(`/add-album/${bandId}`);
 };
+
+
+const showModal = ref(false);
+const albumToDelete = ref<number | null>(null)
+
+const confirmDelete = (id: number) => {
+  albumToDelete.value = id
+  showModal.value = true
+};
+
+const deleteAlbum = async (id: number) => {
+  try {
+    await fetch(`${config.public.apiBase}/albums/${albumToDelete.value}`, {
+      method: "DELETE",
+    });
+    albums.value = albums.value.filter((album) => album.id !== albumToDelete.value)
+  } catch (e) {
+    console.error("Erro ao apagar album:", e)
+  } finally {
+    showModal.value = false;
+    albumToDelete.value = null;
+  }
+}
+
 
 </script>
 
@@ -44,13 +71,25 @@ const goToAddAlbum = () => {
             <v-card-title>{{ album.title }}</v-card-title>
             <v-card-actions>
                 <v-btn color="primary">Ver Sons</v-btn>
-                   <v-btn color="red">Apagar</v-btn>
+                   <v-btn color="red" @click="confirmDelete(album.id)">Apagar</v-btn>
                 </v-card-actions>
           </v-card>
         </v-col>
       </v-row>
     </v-card>
   </v-container>
+
+  <v-dialog v-model="showModal" max-width="400">
+    <v-card>
+      <v-card-title class="text-h5">Confirmar acao</v-card-title>
+      <v-card-text>Tem a certeza que quer apagar este album?</v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="grey" @click="showModal=false">Cancelar</v-btn>
+        <v-btn color="red" @click="deleteAlbum">Apagar</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
   
 </template>
 
