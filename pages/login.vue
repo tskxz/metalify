de2aff950ba6651fe7c1f9b7d5af971cea7a5509
Tanjil <tanjilkh@gmail.com>
2025-03-04 18:35:42 +0000

    web: create login page

---
+++ pages/login.vue
@@ -0,0 +1,69 @@
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const email = ref("");
+const password = ref("");
+const errorMessage = ref("");
+const loading = ref(false);
+
+const login = async () => {
+  errorMessage.value = "";
+  loading.value = true;
+
+  try {
+    const { data, error } = await useFetch("/api/auth/login", {
+      method: "POST",
+      body: { email: email.value, password: password.value },
+    });
+
+    if (error.value) {
+      throw new Error(error.value.statusMessage);
+    }
+
+    // TODO: Use http-only cookie instead of this, this is unsafe...
+    localStorage.setItem("token", data.value.token);
+
+    router.push("/");
+  } catch (error) {
+    errorMessage.value = error.message;
+  } finally {
+    loading.value = false;
+  }
+};
+</script>
+
+<template>
+  <v-container class="d-flex justify-center align-center" style="height: 100vh">
+    <v-card width="400" class="pa-5">
+      <v-card-title class="text-center">Login</v-card-title>
+
+      <v-card-text>
+        <v-alert v-if="errorMessage" type="error" class="mb-4">
+          {{ errorMessage }}
+        </v-alert>
+
+        <v-form @submit.prevent="login">
+          <v-text-field
+            label="Email"
+            v-model="email"
+            type="email"
+            required
+          ></v-text-field>
+
+          <v-text-field
+            label="Password"
+            v-model="password"
+            type="password"
+            required
+          ></v-text-field>
+
+          <v-btn type="submit" color="primary" block :loading="loading">
+            Entrar
+          </v-btn>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
