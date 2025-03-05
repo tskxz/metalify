329aefb6e10bb340a2bfe6592333da946ed38a51
Tanjil <tanjilkh@gmail.com>
2025-03-05 10:56:34 +0000

    web: redirect to home if user is auth

--- pages/login.vue
+++ pages/login.vue
@@ -5,11 +5,27 @@ import { useAuthStore } from "@/stores/auth";
 
 const authStore = useAuthStore();
 
+const {username, userID} = storeToRefs(authStore);
+
+authStore.loadUserFromLocalStorage();
+
+const isAuthenticated = computed(() => !!username.value);
+const isLoading = ref(true);
+
 const router = useRouter();
 const email = ref("");
 const password = ref("");
 const errorMessage = ref("");
 const loading = ref(false);
+
+onMounted(() => {
+  if (isAuthenticated.value) {
+    router.push("/");
+  } else {
+    isLoading.value = false;
+  }
+});
+
 
 const login = async () => {
   errorMessage.value = "";
@@ -37,35 +53,43 @@ const login = async () => {
 </script>
 
 <template>
-  <v-container class="d-flex justify-center align-center" style="height: 100vh">
-    <v-card width="400" class="pa-5">
-      <v-card-title class="text-center">Login</v-card-title>
+  <template v-if="isLoading">
+    <v-container class="d-flex justify-center align-center" style="height: 100vh;">
+      <v-progress-circular indeterminate color="primary" size="64"></v-progress-circular>
+    </v-container>
+  </template>
+  
+   <template v-else>
+    <v-container class="d-flex justify-center align-center" style="height: 100vh">
+      <v-card width="400" class="pa-5">
+        <v-card-title class="text-center">Login</v-card-title>
 
-      <v-card-text>
-        <v-alert v-if="errorMessage" type="error" class="mb-4">
-          {{ errorMessage }}
-        </v-alert>
+        <v-card-text>
+          <v-alert v-if="errorMessage" type="error" class="mb-4">
+            {{ errorMessage }}
+          </v-alert>
 
-        <v-form @submit.prevent="login">
-          <v-text-field
-            label="Email"
-            v-model="email"
-            type="email"
-            required
-          ></v-text-field>
+          <v-form @submit.prevent="login">
+            <v-text-field
+              label="Email"
+              v-model="email"
+              type="email"
+              required
+            ></v-text-field>
 
-          <v-text-field
-            label="Password"
-            v-model="password"
-            type="password"
-            required
-          ></v-text-field>
+            <v-text-field
+              label="Password"
+              v-model="password"
+              type="password"
+              required
+            ></v-text-field>
 
-          <v-btn type="submit" color="primary" block :loading="loading">
-            Entrar
-          </v-btn>
-        </v-form>
-      </v-card-text>
-    </v-card>
-  </v-container>
+            <v-btn type="submit" color="primary" block :loading="loading">
+              Entrar
+            </v-btn>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-container>
+  </template>
 </template>
