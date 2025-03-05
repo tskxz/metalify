d927b78b8dc8f79b81dcd1a59460ea02e9eb9594
Tanjil <tanjilkh@gmail.com>
2025-03-05 10:34:40 +0000

    web: verify if user is authenticated

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,23 +1,27 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, computed, onMounted } from "vue";
 import { useRouter } from "vue-router";
-import {storeToRefs} from "pinia"
-import {useAuthStore} from "@/stores/auth"
+import { storeToRefs } from "pinia";
+import { useAuthStore } from "@/stores/auth";
 
-const authStore = useAuthStore()
-const {username, userID} = storeToRefs(authStore);
-
-authStore.loadUserFromLocalStorage();
+const authStore = useAuthStore();
+const { username } = storeToRefs(authStore);
 
 const isLoading = ref(true);
 
 const router = useRouter();
 
+const isAuthenticated = computed(() => !!username.value);
+
+onMounted(() => {
+  authStore.loadUserFromLocalStorage();
+  isLoading.value = false;
+});
+
 const logout = () => {
-  authStore.logout()
+  authStore.logout();
   router.push("/login");
 };
-
 </script>
 
 <template>
@@ -29,15 +33,20 @@ const logout = () => {
       <v-toolbar-title>Metalify</v-toolbar-title>
     </router-link>
     <v-spacer></v-spacer>
-    <template v-if="username !== null">
-      <template v-if="username">
+
+    <template v-if="isLoading">
+      <v-btn color="white" disabled>A carregar...</v-btn>
+    </template>
+
+    <template v-else>
+      <template v-if="isAuthenticated">
         <v-btn color="white">Bem-vindo, {{ username }}</v-btn>
         <v-btn @click="logout" color="white">Sair</v-btn>
       </template>
-      <template v-else>
-        <v-btn to="/login" color="white">Login</v-btn>
-      </template>
+      
+      <v-btn v-else to="/login" color="white">Login</v-btn>
     </template>
+
     <v-btn to="/add-band" color="white">➕ Adicionar Banda</v-btn>
   </v-app-bar>
 </template>
